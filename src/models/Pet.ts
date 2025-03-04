d3dac46b24cc1aec3fd4b4293eb1cc7808ba9e85
Atd27 <y518a480@gmail.com>
2025-03-04 23:30:41 +0900

    Petスキーマ修正

--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -1,14 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const petSchema = new Schema({
-  name: String,
-  photoUrls: String,
-  status: String,
-  categoriesId: {
-    type: Schema.Types.ObjectId,
-    required: true,
+const petSchema = new Schema(
+  {
+    name: String,
+    photoUrls: String,
+    status: String,
+    categoriesId: {
+      type: Schema.Types.ObjectId,
+      default: null,
+    },
+    tagsId: { type: Schema.Types.ObjectId, default: null },
   },
-  tagsId: { type: Schema.Types.ObjectId, required: true },
-});
+  { collection: "pet" }
+);
 
 export const Pet = mongoose.model("Pet", petSchema);
