ea431f2c2b1c5cb069c32d071ec91f40b42c68de
Atd27 <y518a480@gmail.com>
2025-03-06 15:08:16 +0900

    delete api作成

--- src/routes/pet/index.ts
+++ src/routes/pet/index.ts
@@ -94,33 +94,34 @@ router.get("/pets/:id", async (req: Request, res: Response) => {
 });
 
 // POST pet/{petid}
+// router.post("/pets/:id",async(req:Request,res:Response) => {
+
+// })
 
 // DELETE pet/{petid}
-// router.delete(
-//   "/pets/:id",
-//   async (req: Request, res: Response): Promise<void> => {
-//     try {
-//       const petId = req.params.id;
+router.delete(
+  "/pets/:id",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const petId = req.params.id;
 
-//       const objectId = new ObjectId(petId);
+      const objectId = new ObjectId(petId);
 
-//       const result = await Pet.deleteOne({ _id: objectId });
+      const result = await Pet.deleteOne({ _id: objectId });
 
-//       if (result.deletedCount === 0) {
-//         res
-//           .status(404)
-//           .json({ error: "指定されたIDのペットが見つかりませんでした" });
-//         return;
-//       }
+      if (result.deletedCount === 0) {
+        res
+          .status(404)
+          .json({ error: "指定されたIDのペットが見つかりませんでした" });
+        return;
+      }
 
-//       res.status(200).json({ message: "ペットデータ削除成功", data: result });
-//     } catch (error) {
-//       console.error("削除エラー", (error as Error).message);
-//       res.status(500).json({ error: "ペットデータ削除失敗" });
-//     }
-//   }
-// );
-
-//
+      res.status(200).json({ message: "ペットデータ削除成功", data: result });
+    } catch (error) {
+      console.error("削除エラー", (error as Error).message);
+      res.status(500).json({ error: "ペットデータ削除失敗" });
+    }
+  }
+);
 
 export default router;
